424c71e48b7bfe24963a4ba5e49d2e7719d37afa
Artyom Rudakov <two_dollars@icloud.com>
2020-01-14 15:41:37 +0200

    Adds Package.swift

---
+++ Package.swift
@@ -0,0 +1,25 @@
+// swift-tools-version:5.0
+
+import PackageDescription
+
+let package = Package(
+    name: "GhostTypewriter",
+    platforms: [.iOS(.v10)],
+    products: [
+        .library(
+            name: "GhostTypewriter",
+            targets: ["GhostTypewriter"]
+        ),
+    ],
+    targets: [
+        .target(
+            name: "GhostTypewriter",
+            path: "GhostTypewriter"
+        ),
+        .testTarget(
+            name: "GhostTypewriterTests",
+            dependencies: ["GhostTypewriter"],
+            path: "GhostTypewriterTests"
+        )
+    ]
+)
